67f1f624ad792c013a5d5eb58f89eb9e0ac3eeaf
kidp2h <nthinh.dev@gmail.com>
2026-05-09 09:08:26 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  basePath: '/gpt-board',
+  assetPrefix: '/gpt-board',
 };
 
 export default nextConfig;
